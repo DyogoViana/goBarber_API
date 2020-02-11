b0994ad55ac73504f5719e81671d1a87dddbfaf9
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-02-10 22:42:27 -0300

    Configurando Sequelize.

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'freedom',
+    database: 'gobarber',
+
+    define: {
+        timestamps: true,
+        underscore: true,
+        underscoreAll: true,
+    },
+};
