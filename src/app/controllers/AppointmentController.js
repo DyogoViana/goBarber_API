8415e03b56cdf91c091e2a7b80030dfc86dfa7b1
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-04-02 21:00:52 -0300

    Validações de agendamento [BUG]

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,14 +1,15 @@
-// AappointmentController.js
+// AppointmentController.js
 
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
-class AappointmentController {
+class AppointmentController {
     async store(request, response) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
-            date: Yup.date().required(),
+            date: Yup.date(),
         });
 
         if (!(await schema.isValid(request.body))) {
@@ -24,18 +25,42 @@ class AappointmentController {
 
         if (!checkIsProvider) {
             return response.status(401).json({
-                error: 'you can only create appointment with prooviders',
+                error: 'you can only create appointment with providers',
             });
+        }
+
+        // Verifica se é uma data passada. Arredonda a hora, ex: 16:00:00
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return response
+                .status(400)
+                .json({ error: 'Past dates are not permitted' });
+        }
+
+        // Verifica se a disponibilidade.
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return response
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
         }
 
         const appointment = await Appointment.create({
             user_id: request.userId,
             provider_id,
-            date,
+            date: hourStart,
         });
 
         return response.json(appointment);
     }
 }
 
-export default new AappointmentController();
+export default new AppointmentController();
