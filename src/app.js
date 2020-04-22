b0b5eae4b83f84eb597a4cd1b1300a57fa3bc1cf
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-04-22 20:06:35 -0300

    Tratamento de exceções com o "sentry" e o "Youch".

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 // Importações
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
     constructor() {
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use(
             '/files',
@@ -22,6 +30,16 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    // Quando um middleware recebe 4 parâmetros, ele reconhece como um middleware de tratamento de exceções.
+    exceptionHandler() {
+        this.server.use(async (error, request, response, next) => {
+            const errors = await new Youch(error, request).toJSON();
+
+            return response.status(500).json(errors);
+        });
     }
 }
 
