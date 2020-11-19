b71bbf51c4b64c76d538cf1188ce0e41280612bb
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-11-19 11:55:04 -0300

    ajustes na API para acessar no projeto.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-// Importações
 import 'dotenv/config';
+
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -22,6 +23,7 @@ class App {
 
     middlewares() {
         this.server.use(Sentry.Handlers.requestHandler());
+        this.server.use(cors()); // Controla o acesso a API
         this.server.use(express.json());
         this.server.use(
             '/files',
